677f1a444be1a278b94b41463b0f8542fc4f2ba3
Zynro <33652950+Zynro@users.noreply.github.com>
2019-04-11 19:10:19 -0400

    Adding basic starting values and return functions.

--- src/battleship/Ocean.java
+++ src/battleship/Ocean.java
@@ -1,5 +1,7 @@
 package battleship;
 
+import java.util.Random;
+
 public class Ocean {
 
 	private Ship[][] ships = new Ship[10][10];
@@ -8,19 +10,43 @@ public class Ocean {
 	private int shipsSunk;
 	
 	public Ocean() {
-		
+		for(int i = 0; i<10;i++) {
+			for(int b = 0; b<10;b++) {
+				ships[i][b] = new EmptySea();
+			}
+		}
+		shotsFired = 0;
+		hitCount = 0;
+		shipsSunk = 0;
+	}
+	
+	private int determineDirection() {
+		Random random = new Random();
+		int direction = random.nextInt(1);
+		return direction;
 	}
 	
 	void placeAllShipsRandomly() {
 		
+		
 	}
 	
-	boolean isOccupied(int row, int column) {
-		return false;
+	boolean isOccupied(int column, int row) {
+		if(ships[column][row].getClass().getSimpleName() != "EmptySea") {
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
-	boolean shootAt(int row, int column) {
-		return false;
+	boolean shootAt(int column, int row) {
+		shotsFired++;
+		if((ships[column][row].getClass().getSimpleName() != "EmptySea") && (ships[column][row].isSunk() == false)) {
+			hitCount++;
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 	int getShotsFired() {
@@ -36,7 +62,11 @@ public class Ocean {
 	}
 	
 	boolean isGameOver() {
-		return false;
+		if (this.shipsSunk == 10){
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 	Ship[][] getShipArray() {
