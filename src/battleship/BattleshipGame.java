3502fdb56b676144991049649bbb85b2f5f4f8f9
Monil Ghodasra <monilg@seas.upenn.edu>
2019-04-16 14:12:24 -0400

    Added scanner

--- src/battleship/BattleshipGame.java
+++ src/battleship/BattleshipGame.java
@@ -1,11 +1,20 @@
 package battleship;
 
+import java.util.Scanner;
+
 public class BattleshipGame {
 
 	public static void main(String[] args) {
 		Ocean ocean = new Ocean();
 		ocean.placeAllShipsRandomly();
 		ocean.printHidden();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter row:");
+		int row = sc.nextInt();
+		System.out.println("Enter column:");
+		int column = sc.nextInt();
+		
 	}
 
 }
