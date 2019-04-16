997aff4cb963ae10a01aead572d467e695f7fe2b
Zynro <33652950+Zynro@users.noreply.github.com>
2019-04-16 13:01:53 -0400

    Complete implementation of Ocean Class Missing: Javadocs TODO: Resolve placeShipsRandomly method (currently infinite looping)

--- src/battleship/BattleshipGame.java
+++ src/battleship/BattleshipGame.java
@@ -3,8 +3,9 @@ package battleship;
 public class BattleshipGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Ocean ocean = new Ocean();
+		ocean.placeAllShipsRandomly();
+		ocean.print();
 	}
 
 }
--- src/battleship/Ocean.java
+++ src/battleship/Ocean.java
@@ -5,43 +5,108 @@ import java.util.Random;
 public class Ocean {
 
 	private Ship[][] ships = new Ship[10][10];
+	private boolean[][] hitArray = new boolean[10][10];
 	private int shotsFired;
 	private int hitCount;
 	private int shipsSunk;
+	
 	
 	public Ocean() {
 		for(int i = 0; i<10;i++) {
 			for(int b = 0; b<10;b++) {
 				ships[i][b] = new EmptySea();
+				hitArray[i][b] = false;
 			}
 		}
-		shotsFired = 0;
-		hitCount = 0;
-		shipsSunk = 0;
+		this.shotsFired = 0;
+		this.hitCount = 0;
+		this.shipsSunk = 0;
 	}
-	
-	private int determineDirection() {
-		Random random = new Random();
-		int direction = random.nextInt(1);
-		return direction;
-	}
-	
+	/**
+	 * 
+	 */
 	void placeAllShipsRandomly() {
+		Random random = new Random();
+		boolean placed = false;
+		boolean horizontal;
 		
+		Ship battleship = new Battleship(4);
+		Ship[] cruisers = new Cruiser[2]; 
+		Ship[] destroyers = new Destroyer[3];
+		Ship[] submarines = new Submarine[4];
+		
+		while(placed = false) {
+			int column = random.nextInt();
+			int row = random.nextInt();
+			horizontal = random.nextBoolean();
+			if (battleship.okToPlaceShipAt(row, column, horizontal, this) == true) {
+				battleship.placeShipAt(row, column, horizontal, this);
+				placed = true;
+			}else {
+				continue;
+			}
+		}	
+		
+		for(int i = 0; i<cruisers.length; i++) {
+			placed = false;
+			cruisers[i] = new Cruiser(3);
+			while(placed == false) {
+				int column = random.nextInt();
+				int row = random.nextInt();
+				horizontal = random.nextBoolean();
+				if (cruisers[i].okToPlaceShipAt(row, column, horizontal, this) == true) {
+					cruisers[i].placeShipAt(row, column, horizontal, this);
+					placed = true;
+				}else {
+					continue;
+				}
+			}
+		}
+		for(int i = 0; i<destroyers.length; i++) {
+			placed = false;
+			destroyers[i] = new Destroyer(3);
+			while(placed == false) {
+				int column = random.nextInt();
+				int row = random.nextInt();
+				horizontal = random.nextBoolean();
+				if (destroyers[i].okToPlaceShipAt(row, column, horizontal, this) == true) {
+					destroyers[i].placeShipAt(row, column, horizontal, this);
+					placed = true;
+				}else {
+					continue;
+				}
+			}
+		}
+		for(int i = 0; i<submarines.length; i++) {
+			placed = false;
+			submarines[i] = new Submarine(3);
+			while(placed == false) {
+				int column = random.nextInt();
+				int row = random.nextInt();
+				horizontal = random.nextBoolean();
+				if (submarines[i].okToPlaceShipAt(row, column, horizontal, this) == true) {
+					submarines[i].placeShipAt(row, column, horizontal, this);
+					placed = true;
+				}else {
+					continue;
+				}
+			}
+		}
 		
 	}
 	
-	boolean isOccupied(int column, int row) {
-		if(ships[column][row].getClass().getSimpleName() != "EmptySea") {
+	boolean isOccupied(int row, int column) {
+		if(ships[row][column].getClass().getSimpleName() != "EmptySea") {
 			return true;
 		}else {
 			return false;
 		}
 	}
 	
-	boolean shootAt(int column, int row) {
+	boolean shootAt(int row, int column) {
 		shotsFired++;
-		if((ships[column][row].getClass().getSimpleName() != "EmptySea") && (ships[column][row].isSunk() == false)) {
+		if((ships[row][column].getClass().getSimpleName() != "EmptySea") && (ships[column][row].isSunk() == false)) {
+			hitArray[row][column] = true;
 			hitCount++;
 			return true;
 		}else {
@@ -74,7 +139,28 @@ public class Ocean {
 	}
 	
 	void print() {
-		
+		String output = "";
+		String[] printArray = new String[11];
+		for(int i=0;i<printArray.length;i++) {
+			if(i==0) {
+				printArray[i] = "  0 1 2 3 4 5 6 7 8 9";
+				continue;
+			}else {
+				try {
+					printArray[i] = Integer.toString(i-1);
+					for (int column=0;column<ships[i-1].length;column++) {
+						if (hitArray[i-1][column] == false){
+							printArray[i] = printArray[i] + " " + ".";
+						}else {
+							printArray[i] = printArray[i] + " " + ships[i-1][column].toString();
+						}
+					}
+				}
+				catch (Exception e) {}
+			}
+		}
+		output = String.join("\n", printArray);
+		System.out.println(output);
 	}
 	
 }
